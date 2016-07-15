fd8c5ebb68d7aff0a91cea0b8ee77f39846467de
honbobau <c.hon.ip@gmail.com>
2016-07-14 20:09:54 -0400

    global recipe feed displayed, not linked to anything

--- src/components/utility_components/RecipeCard.jsx
+++ src/components/utility_components/RecipeCard.jsx
@@ -5,15 +5,17 @@ class RecipeCard extends React.Component {
 
   render() {
     const recipe = this.props.recipe;
-    const instructions = recipe.instructions;
-    const ingredients = recipe.ingredients;
-    const description = recipe.description;
+    const instructions = recipe[0].recipe.instructions;
+    const ingredients = recipe[0].recipe.ingredients;
+    const description = recipe[0].recipe.description;
+    const imageURL = recipe[0].photos[0].filepath;
 
     console.log(recipe);
 
     return( 
       <div> 
-        {instructions}
+        <img src={imageURL} />
+        <p>{description}</p>
       </div>
     )
   }
