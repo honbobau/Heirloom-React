dda528e1405d4fa9942a257af4cb06c4a207eb67
honbobau <c.hon.ip@gmail.com>
2016-07-19 15:56:43 -0400

    css changes

--- src/components/user_profile_components/RecipeGalleryNav.jsx
+++ src/components/user_profile_components/RecipeGalleryNav.jsx
@@ -6,14 +6,14 @@ class RecipeGalleryNav extends React.Component {
     let toggleGallery = this.props.toggleGallery;
 
     return(
-      <div>
-        <div>
+      <div className='profile-gallery-nav'>
+        <div className='profile-gallery-nav-btn'>
           <span className='icon is-medium'>
             <i className='fa fa-user' onClick={ () => toggleGallery('user') }></i>
           </span>
         </div>
 
-        <div>
+        <div className='profile-gallery-nav-btn'>
           <span className='icon is-medium'>
             <i className='fa fa-heart' onClick={ () => toggleGallery('fav') }></i>
           </span>
