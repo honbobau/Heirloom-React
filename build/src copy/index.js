478503e2dc26909926cfa38764e5dfda8211e55f
wshanks01 <william.shanks2@gmail.com>
2016-07-20 18:38:08 -0400

    fixed header and card size

---
+++ build/src copy/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import LandingPage from './components/landing_page_components/LandingPage.jsx';
+import SignupPage from './components/signup_components/SignupPage.jsx';
+import UserFeedPage from './components/user_feed_components/UserFeedPage.jsx';
+import GlobalFeedPage from './components/global_feed_components/GlobalFeedPage.jsx';
+import RecipeNewEditPage from './components/recipe_new_edit_components/RecipeNewEditPage.jsx';
+import RecipePage from './components/recipe_components/RecipePage.jsx';
+import ProfilePage from './components/user_profile_components/ProfilePage.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
